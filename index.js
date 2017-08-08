710af21d168ce89364808bd74231704193ed569f
Jonathan Lara <j.lara@signot.com>
2017-08-07 19:12:57 -0500

    remover sociedades, abreviaturas de razon social, remover o remplzar caracteres especiales remover cia. y soc.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 'use strict';
+const personaMoral = require('./lib/personaMoral'); 
+const personaFisica = require('./lib/personaFisica'); 
+
+const infoPersonaMoral = {
+    razonSocial: 'cia. 13 el ARROZ @ C@FE ALGO, S. en N.C.',
+    fechaConstitucion: new Date()
+}
+/**
+ * 
+ * @param {string} tipo 
+ * @param {any} data 
+ */
+const  generar = (tipo, data) =>
+    tipo === 'moral' ? personaMoral.generarRFC(data) :
+    tipo === 'fisica' ? personaFisica.generarRFC(data) :
+    `${tipo} no es tipo de persona valido. indique si es moral o fisica`
+
+console.log(generar('moral', infoPersonaMoral));
+ 
